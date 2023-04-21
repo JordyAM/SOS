b6fdd454a6b6db8c42666f919d2be512d5b30a11
JordyAM <JAlexanderMcLean@Gmail.com>
2023-04-21 07:15:56 -0400

    setting up tesseract

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { createWorker } from 'tesseract.js';
 
 function App() {
+  const worker = createWorker({
+    logger: m => console.log(m)
+  });
+   const doOCR = async () => {
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng'); 
+    const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+    await worker.terminate();
+    setOcr(text);
+  };
+  const [ocr, setOcr] = useState('Recognizing text...');
+  useEffect(() => {
+    doOCR();
+  }, [ocr]) 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{ocr}</p>
     </div>
   );
 }
