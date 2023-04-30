ad4a21b53ca51fe4dcb136574b87090c334947c1
JordyAM <JAlexanderMcLean@Gmail.com>
2023-04-30 14:42:29 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import './App.css';
 import { createWorker } from 'tesseract.js';
 
 function App() {
-  const worker = createWorker({
-    logger: m => console.log(m)
-  });
-   const doOCR = async () => {
-    await worker.load();
-    await worker.loadLanguage('eng');
-    await worker.initialize('eng'); 
-    const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
-    await worker.terminate();
-    setOcr(text);
-  };
+  
+   
   const [ocr, setOcr] = useState('Recognizing text...');
   useEffect(() => {
+    const { createWorker }  = require("tesseract.js");
+    const worker = createWorker({
+      logger: m => console.log(m)
+    });
+    const doOCR = async () => {
+      await worker.load();
+      await worker.loadLanguage('eng');
+      await worker.initialize('eng'); 
+      const { data: { text } } = await worker.recognize('eng_bw.png');
+      console.log(text); 
+      await worker.terminate();
+      setOcr(text);
+    };
     doOCR();
-  }, [ocr]) 
+  },[ocr] ) 
   return (
     <div className="App">
       <p>{ocr}</p>
